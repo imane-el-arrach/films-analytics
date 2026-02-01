df168f602dd43c4724eb1527a67aca73687242dd
imane-el-arrach <imanearrache2@gmail.com>
2026-02-01 18:28:24 +0100

    This page relies on a local FastAPI backend. It is fully functional in local development.

--- streamlit_app/page3.py
+++ streamlit_app/page3.py
@@ -3,7 +3,8 @@ import pandas as pd
 from pathlib import Path
 import re
 
- ##### ====== Mise en cache de l'initialisation du Client ===
+
+"""##### ====== Mise en cache de l'initialisation du Client ===
 from imane_moviessdk import MovieClient, MovieConfig
 
 @st.cache_resource
@@ -135,3 +136,4 @@ if st.button("🔍 Lancer la recherche"):
 
 else:
     st.info("🧭 Définissez vos filtres, puis cliquez sur **Lancer la recherche** pour explorer les films.")
+"""
